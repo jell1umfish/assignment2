664452945004616611e926c7c218a65b1d5b4bfc
jell1umfish <jell1umfish@gmail.com>
2023-04-19 23:00:06 +0600

    MyLinkedList class that implements MyList and contains methods of LinkedList elements
    defined a private inner class Node
    
    head references the first node in the list.
    tail references the last node in the list
    size keep track of the number of elements in the list
    
    add method creates new Node class element, that contains given element and dont have next element(null) and the tail;
    if our LinkedList is empty, isEmpty method returns true, and we say that head equals to newNode. If its not: we say that next tail is newNode. Then we make our size +1;
    
    ill be sure, this method was hard to me. So, at first we check if index is in range of list. then we create Node "currentNode" and if index is less than half of size, we say that it equals to head; else we consider currentNode as tail;
    
    contains method check if given object equals to one of elements of list.
    
    i didnt get how to do sort() method

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -83,7 +83,38 @@ public class MyLinkedList<T> implements MyList<T> {
         size--;
         return nodeToRemove.element;
     }
-
+    public boolean remove(T item) {
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(item)) {
+                if (current == head) {
+                    // removing the first element
+                    head = current.next;
+                    if (head != null) {
+                        head.prev = null;
+                    } else {
+                        tail = null;
+                    }
+                } else if (current == tail) {
+                    // removing the last element
+                    tail = current.prev;
+                    if (tail != null) {
+                        tail.next = null;
+                    } else {
+                        head = null;
+                    }
+                } else {
+                    // removing an element in the middle
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
     public int size(){
         return size;
     }
@@ -97,6 +128,70 @@ public class MyLinkedList<T> implements MyList<T> {
             currentNode = currentNode.next;
         }
         return false;
+    }
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node newNode = new Node(item, null, null);
+        if (size == 0) {
+            head = tail = newNode;
+        } else if (index == 0) {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else if (index == size) {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            Node currentNode = head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            newNode.next = currentNode;
+            newNode.prev = currentNode.prev;
+            currentNode.prev.next = newNode;
+            currentNode.prev = newNode;
+        }
+
+        size++;
+    }
+    public void sort(){
+        if (head == null || head.next == null) {
+            System.out.println("i didnt get it");
+        }
+    }
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int indexOf(Object o) {
+        int index = 0;
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+    public int lastIndexOf(Object o) {
+        int index = size - 1;
+        Node current = tail;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return index;
+            }
+            current = current.prev;
+            index--;
+        }
+        return -1;
     }
 
     private boolean isEmpty() {
