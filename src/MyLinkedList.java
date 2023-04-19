5606500701fb29df98c3b77dbd929bbca8dc04a4
jell1umfish <jell1umfish@gmail.com>
2023-04-19 20:31:34 +0600

    MyLinkedList class that implements MyList and contains methods of LinkedList elements
    defined a private inner class Node
    
    head references the first node in the list.
    tail references the last node in the list
    size keep track of the number of elements in the list
    
    add method creates new Node class element, that contains given element and dont have next element(null) and the tail;
    if our LinkedList is empty, isEmpty method returns true, and we say that head equals to newNode. If its not: we say that next tail is newNode. Then we make our size +1;

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,13 +1,39 @@
-public class MyLinkedList<T> implements MyList<T>{
+public class MyLinkedList<E> implements MyList<E> {
+
     private class Node {
-        private Object element;
+        private E element;
         private Node next;
         private Node prev;
 
-        public Node(Object element, Node next, Node prev) {
+        public Node(E element, Node next, Node prev) {
             this.element = element;
             this.next = next;
             this.prev = prev;
         }
+    }
 
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    public void add(E element) {
+        Node newNode = new Node(element, null, tail);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
 }
