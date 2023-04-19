266e7696fef4974e1c0425ab8cc09bbe2a1d1c26
jell1umfish <jell1umfish@gmail.com>
2023-04-19 20:15:54 +0600

    MyLinkedList class that implements MyList and contains methods of LinkedList elements
    defined a private inner class Node

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,3 +1,13 @@
 public class MyLinkedList<T> implements MyList<T>{
+    private class Node {
+        private Object element;
+        private Node next;
+        private Node prev;
+
+        public Node(Object element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
 
 }
